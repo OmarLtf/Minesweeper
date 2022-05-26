9df8e032c38ee815900c14ea1b0f6fac0165bf67
OmarLtf <omarltf2001@gmail.com>
2022-05-26 23:23:01 +0100

    board

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ var square2 = document.createElement("div");
 square2.id = "square";
 var body = document.getElementsByTagName("BODY")[0];
 
-square.addEventListener("mousedown", changeSquareRed);
-square.addEventListener("mouseup", changeSquareBlack);
+square.addEventListener("click", changeSquareRed);
+square2.addEventListener("click", changeSquareRed2);
 
 function changeSquareRed() {
-  square.style.backgroundColor = "red";
+  square.style.backgroundColor = generateRandomColor();
   body.appendChild(square2);
 }
 
-function changeSquareBlack() {
-  square.style.backgroundColor = "black";
+function changeSquareRed2() {
+  square2.style.backgroundColor = generateRandomColor();
+  body.appendChild(square);
+}
+
+function generateRandomColor() {
+  let maxVal = 0xffffff; // 16777215.
+  let randomNumber = Math.random() * maxVal;
+  randomNumber = Math.floor(randomNumber);
+  randomNumber = randomNumber.toString(16);
+  let randColor = randomNumber.padStart(6, 0);
+  return `#${randColor.toUpperCase()}`;
 }
